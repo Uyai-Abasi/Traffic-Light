75385678b60b5448d61f9026d73d248a7aa1e928
Uyai-Abasi <isidoreuyaiabasi@gmail.com>
2022-06-21 08:43:52 -0700

    first-commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// if else
+
+alert("traffic light sign")
+let input = prompt("follow the Signal")
+var firstColor = "red"
+var secondColor = "yellow"
+var thirdColor = "green"
+if (firstColor == input) {
+    alert("STOP")
+}
+else if (secondColor == input) {
+    alert("WAIT")
+    alert("GET READY")
+}
+else if (thirdColor == input) {
+    alert("GO")
+}
+else {
+    alert("OOPS!")
+}
